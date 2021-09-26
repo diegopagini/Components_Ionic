f58bf432ba63136d0e1285e7aa14eaa7ef02dc3b
Diego Pagini <diegopaginidev@gmail.com>
2021-09-26 21:39:53 +0200

    routes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public components: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet',
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirecTo: '/alert',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit(): void {}
 }
